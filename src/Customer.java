c3f9167408ab83016c9bfff93e3cbba727df8573
Karl Spickermann <karl.spickermann@web.de>
2017-05-10 16:00:24 +0200

    extracted renterpoints

--- src/Customer.java
+++ src/Customer.java
@@ -25,12 +25,7 @@ class Customer {
             
             Rental each = (Rental) enum_rentals.nextElement();
             
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
-				frequentRenterPoints ++;
-			}
+            frequentRenterPoints = getFrequentRenterPoints(frequentRenterPoints, each);
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.amountFor()) + "\n";
             totalAmount += each.amountFor();
@@ -40,6 +35,15 @@ class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
+	private int getFrequentRenterPoints(int frequentRenterPoints, Rental each) {
+		// add frequent renter points
+		frequentRenterPoints ++;
+		// add bonus for a two day new release rental
+		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
+			frequentRenterPoints ++;
+		}
+		return frequentRenterPoints;
+	}
 
 }
     
