bf89cfdb7a422f9c3e2ba29cba9146df2d00fbf6
Karl Spickermann <karl.spickermann@sap.com>
2017-05-10 15:20:26 +0200

    Added brackets to control statements for easy readiability

--- src/JUnitTest.java
+++ src/JUnitTest.java
@@ -1,7 +1,11 @@
 
 
+
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+@RunWith(JUnit4.class)
 
 public class JUnitTest {
 	
