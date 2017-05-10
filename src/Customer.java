a6aa576049d7961fe50c84b136c6421c2383291d
Karl Spickermann <karl.spickermann@web.de>
2017-05-10 16:23:04 +0200

    Replaced Temps with query methods

--- src/Customer.java
+++ src/Customer.java
@@ -15,8 +15,7 @@ class Customer {
         return name;
     };
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
+        
         Enumeration enum_rentals = rentals.elements();	    
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
@@ -25,16 +24,35 @@ class Customer {
             
             Rental each = (Rental) enum_rentals.nextElement();
             
-            frequentRenterPoints += each.getFrequentRenterPoints();
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.amountFor()) + "\n";
-            totalAmount += each.amountFor();
+            
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
     }
+    private double getTotalCharge() {
+    	 double result = 0;
+    	 Enumeration enum_rentals = rentals.elements();
+    	 while (enum_rentals.hasMoreElements()) {
+    	 Rental each = (Rental) enum_rentals.nextElement();
+    	 result += each.amountFor();
+    	 }
+    	 return result;
+    	 }
+    private int getTotalFrequentRenterPoints(){
+    	 int result = 0;
+    	 Enumeration enum_rentals = rentals.elements();
+    	 while (enum_rentals.hasMoreElements()) {
+    	 Rental each = (Rental) enum_rentals.nextElement();
+    	 result += each.getFrequentRenterPoints();
+    	 }
+    	 return result;
+    	 }
+
+
 
 }
     
